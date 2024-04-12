d5502f2e15c31999afc7a12bbf5d2c4a39f14fbb
mbebe <drazah@o2.pl>
2024-04-12 12:12:17 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@
 import { writeFile } from "node:fs/promises"
 import { assert } from "node:console"
 
-var tools = require("javascript-deobfuscator/src/index.ts");
+const tools = require("javascript-deobfuscator/src/index.ts");
 const source = "abcdefgh"
 const output = tools.deobfuscate(source);
 console.info("Success!")
