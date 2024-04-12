21528f98dc29aab1dd0a241f6b49e9b2270f6f49
mbebe <drazah@o2.pl>
2024-04-12 13:00:39 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import { deobfuscate } from 'javascript-deobfuscator/src/index';
+import { deobfuscate } from 'js-deobfuscator/index';
 import { writeFile } from "node:fs/promises"
 import { assert } from "node:console"
 
