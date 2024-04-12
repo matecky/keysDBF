b4eb579ccde526838b1f65310cf148cd2189ed94
mbebe <drazah@o2.pl>
2024-04-12 14:03:03 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,9 +2,54 @@ import { deobfuscate } from "js-deobfuscator";
 import { writeFile } from "node:fs/promises"
 import { assert } from "node:console"
 import { deobfuscate as dbf} from 'obfuscator-io-deobfuscator'
-import pkg from 'obfuscator-io-deobfuscator';
-const { Deobfuscator } = pkg;
-const source = "abcdefgh"
-const output = deobfuscate(source);
-console.info("Success!")
 
+
+const checkDeobfs = (x) => x.indexOf("<video />") !== -1
+
+// See https://github.com/Claudemirovsky/worstsource-keys/issues/2
+function getCodeVersion() {
+    // [hour]:00:10
+    const versionDate = new Date()
+    versionDate.setMinutes(0)
+    versionDate.setSeconds(10)
+    // Get only the first 10 digits
+    const timestamp = versionDate.getTime().toString().substring(0, 10)
+    return parseInt(timestamp).toString(16) // Convert to HEX
+}
+
+async function getDeobfuscatedScript() {
+//    const vidplayHost = "https://vidplay.lol"
+//    const vidplayHost = "https://vidplay.site"
+    const vidplayHost = "https://mcloud.bz"
+    const headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/120.0",
+        "Referer": vidplayHost + "/e/",
+        "Origin": vidplayHost
+    }
+
+    const scriptUrl = `${vidplayHost}/assets/mcloud/min/embed.js?v=${getCodeVersion()}`
+    const obfuscatedScript = await fetch(scriptUrl, {headers: headers}).then(async (x) => await x.text())
+	
+	const firstTry = await deobfuscate(obfuscatedScript);
+	const secondTry = await dbf(secondTry);
+
+    return secondTry
+}
+
+const deobfuscated = await getDeobfuscatedScript() 
+
+// Phase 4: Let's find the keys!
+if (checkDeobfs(deobfuscated)) {
+    const start = deobfuscated.substring(deobfuscated.indexOf("<video />"))
+    const end = start.substring(0, start.indexOf(".replace"))
+    const keys = Array.from(end.matchAll(/'(\w+)'/g), x => x[1])
+    assert(keys.length == 2, "Invalid array length!")
+
+    // Be happy!
+    console.info("Success!")
+    await writeFile("keys.json", JSON.stringify(keys), "utf8")
+} else {
+    // ... Or not xD
+    console.error("FAIL!")
+    await writeFile("failed.js", deobfuscated, "utf8")
+}
