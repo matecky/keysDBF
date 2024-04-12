085928e6fa74a54b52144d467a6d6c5def020fd0
mbebe <drazah@o2.pl>
2024-04-12 13:51:44 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,8 +1,9 @@
 import { deobfuscate } from "js-deobfuscator";
 import { writeFile } from "node:fs/promises"
 import { assert } from "node:console"
-import { Deobfuscator } from 'obfuscator-io-deobfuscator'
-
+import { deobfuscate } as dbf from 'obfuscator-io-deobfuscator'
+import pkg from 'obfuscator-io-deobfuscator';
+const { Deobfuscator } = pkg;
 const source = "abcdefgh"
 const output = deobfuscate(source);
 console.info("Success!")
